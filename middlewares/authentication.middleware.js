50b79d6adedb3f53815868bd2988401baa8cc25b
Manideep-0164 <peddaboinimanideep03@gmail.com>
2023-10-06 18:32:09 +0530

    added authentication middleware

---
+++ middlewares/authentication.middleware.js
@@ -0,0 +1,44 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const SECRET = process.env.JWT_TKN_SECRET;
+
+const authenticate = async (req, res, next) => {
+  try {
+    const token = req.headers?.authorization;
+    if (!token) return res.status(401).json({ message: "Token not found" });
+
+    // verify JWT token
+    jwt.verify(token, SECRET, (err, decoded) => {
+      // check whether token expired
+      if (err?.name === "TokenExpiredError")
+        return res
+          .status(440)
+          .json({ message: "Session expired, Please login." });
+
+      console.error("JWT verification error:", err.message);
+
+      // check for token errors
+      if (err?.name === "JsonWebTokenError")
+        return res
+          .status(400)
+          .json({ message: "Something went wrong, Please try again later" });
+
+      // get the user info
+      const { userId, email } = decoded;
+
+      req.userId = userId;
+      req.email = email;
+
+      next();
+    });
+  } catch (error) {
+    console.log(`Error authenticating user => ${error}`);
+    res
+      .status(500)
+      .json({ message: "Something went wrong, Please try again later" });
+  }
+};
+
+module.exports = {
+  authenticate,
+};
